5c75a64cffeb05716ea13316344120a855adca0d
Nafiz <nafizuddin1537@gmail.com>
2025-08-25 08:50:35 +0600

    Add files via upload

---
+++ webApp.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.title("--Welcome To Web--")
+st.subheader("This is my first web App")
+st.write("This is made for testing Purpose")
+st.checkbox("Hi")
+st.checkbox("Hello")
+st.checkbox("bye")
+st.checkbox("See You")
+st.text_input(label="",placeholder="whats on your mind?")
